6e8e4ddd70fc61a70256f57de7037ae671ef2cf8
awasthi <you@example.com>
2021-07-13 09:19:22 +0530

    first commit

---
+++ pages/ninjas/test.js
@@ -0,0 +1,10 @@
+const Test = () => {
+    return (  
+        <>
+                <h1>Test</h1>
+                <p>A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language,</p>
+        </>
+    );
+}
+ 
+export default Test;
